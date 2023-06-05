b6ccdefb5759dac15c2f826cc9df602879475b00
Pavel Šuraba <suraba.pavel@gmail.com>
2023-06-05 21:45:09 +0200

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    accentColor: false,
+    textOpacity: false,
+    backgroundOpacity: false,
+    borderOpacity: false,
+    ringColor: false,
+    ringOffsetColor: false,
+    ringOffsetWidth: false,
+    ringOpacity: false,
+    ringWidth: false,
+  },
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Roboto', '-apple-system', 'Arial', 'sans-serif'],
+    },
+    fontWeight: {
+      normal: 400,
+      medium: 500,
+    },
+  },
+  plugins: [],
+}
