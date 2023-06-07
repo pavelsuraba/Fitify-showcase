8bbc7e704de1dbfd04ffb319730c4859cec2f2eb
Pavel Šuraba <suraba.pavel@gmail.com>
2023-06-07 13:16:47 +0200

    icons

--- src/js/components/Dialog.tsx
+++ src/js/components/Dialog.tsx
@@ -1,20 +1,6 @@
 import { Dialog as HeadlessDialog } from '@headlessui/react'
 import { PropsWithChildren } from 'react'
-
-const CloseIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="12"
-    height="12"
-    fill="none"
-    viewBox="0 0 12 12"
-  >
-    <path
-      fill="#9E9E9E"
-      d="M11.627 1.728 7.385 5.97l4.242 4.242-1.414 1.414-4.242-4.242-4.243 4.242-1.414-1.414 4.242-4.242L.314 1.728 1.728.314 5.97 4.556 10.213.314l1.414 1.414Z"
-    />
-  </svg>
-)
+import { CloseIcon } from './Icons'
 
 type Props = {
   isVisible: boolean
--- src/js/components/ExerciseList.tsx
+++ src/js/components/ExerciseList.tsx
@@ -9,27 +9,13 @@ import { ExerciseListItem } from './ExerciseListItem'
 
 import { Dialog } from './Dialog'
 import { ExerciseDetail } from './ExerciseDetail'
+import { SearchIcon } from './Icons'
 
 const getHeadingText = (count: number) => {
   return count > 0
     ? `${count} ${pluralize('exercise', count)}`
     : 'No exercises found'
 }
-
-const SearchIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="12"
-    height="12"
-    fill="none"
-    viewBox="0 0 12 12"
-  >
-    <path
-      fill="#BDBDBD"
-      d="M8.333 7.333h-.526l-.187-.18a4.314 4.314 0 0 0 1.047-2.82 4.333 4.333 0 1 0-4.334 4.334c1.074 0 2.06-.394 2.82-1.047l.18.187v.526l3.334 3.327.993-.993-3.327-3.334Zm-4 0c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3Z"
-    />
-  </svg>
-)
 
 type Props = {
   data: Exercise[]
